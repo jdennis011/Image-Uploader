e0facdcd566d197a4a7527ca8e5d1b184e6411e1
James Dennis <jdennis011@gmail.com>
2021-12-09 23:58:05 +1100

    testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Pages
 import UploadPage from './Components/Pages/UploadPage'
 import UploadedImage from './Components/Pages/UploadedImagePage';
+import ImgServe from './Components/Pages/ImgServe'
 import FZF from './Components/Pages/404Page.js'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={UploadPage} />
         <Route path="/upload/:image_url" component={UploadedImage} />
+        <Route path="/img/:image_url" component={ImgServe} />
         <Route component={FZF} />
       </Switch>
     </Router>
